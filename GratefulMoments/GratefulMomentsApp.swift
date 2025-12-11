972241dd62c61449fca137894f16f51e39500eff
HarrisN1114 <marantz1985@icloud.com>
2025-12-11 14:07:25 -0500

    Initial Commit

---
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by Nathan Harris on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
